bb1e15051c569fcdadb779cbfd3dd1ab8e10ff6d
Oleg Karukov <olegcoder@rambler.ru>
2024-08-14 21:04:59 +0300

    init commit

---
+++ platformer_sln/platformer/platformer.cpp
@@ -0,0 +1,13 @@
+﻿// platformer.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << "Hello CMake." << endl;
+	return 0;
+}
