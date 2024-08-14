4e15793444ec11dfeb55ef32f23436d2c942f880
pavelkaryukov <pavelkaryukov@gmail.com>
2024-08-14 22:10:28 +0300

    см https://github.com/SFML/cmake-sfml-project?tab=readme-ov-file Больше не нужно терпеть унижения, подкладывать dll в бин каталог, согласовывать версию библиотеки, cmake сам выкачает фиксированную версию библиотеки

--- platformer_sln/platformer/platformer.cpp
+++ platformer_sln/platformer/platformer.cpp
@@ -2,12 +2,27 @@
 //
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	cout << "Hello CMake." << endl;
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
 	return 0;
 }
